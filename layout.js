97ba42948b756f05e7962f379dfa383540ab7c8b
Gary <163843362+garyfan1204@users.noreply.github.com>
2025-12-23 08:22:25 +0800

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,25 @@
+$(function() {
+    let btnhtml = 
+    `
+        <a href="./" class="layout" id="toindex">
+            <div>
+                <i class="fa-solid fa-arrow-left"></i>
+            </div>
+        </a>
+        <a href="#" class="layout" id="totop">
+            <div>
+                <i class="fa-solid fa-arrow-up"></i>
+            </div>
+        </a>
+    `
+    $("body").prepend(btnhtml)
+})
+
+
+$(document).on("click", "#totop", function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 0
+    }, 50);
+})
